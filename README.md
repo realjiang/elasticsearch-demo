90dfe6ee62f90cbc3fd2be6a694b5cd9763a679a
superj <superjishere@163.com>
2021-06-14 18:54:13 +0800

    feat:索引相关api测试

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # elasticsearch-demo
 springboot集成es demo
+
+***es版本 7.13.1***
+
+1. 添加依赖,注意版本要修改成和使用版本一致
+
+![自定义es版本](https://gitee.com/superjishere/images/raw/master/img/20210614141635.png)
+![es依赖](https://gitee.com/superjishere/images/raw/master/img/20210614150535.png)
+
+2. 创建配置类 
+
+`com.superj.config.ElasticSearchClientConfig`
+
+3. api测试
+    
+    - 索引相关
+    - 文档相关
+    
+4. 项目demo
--- src/test/java/com/superj/SuperjEsApiApplicationTests.java
+++ src/test/java/com/superj/SuperjEsApiApplicationTests.java
@@ -1,13 +1,54 @@
 package com.superj;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.io.IOException;
 
 @SpringBootTest
 class SuperjEsApiApplicationTests {
 
+    @Resource
+    RestHighLevelClient client;
+
+    // 测试创建索引  CreateIndexRequest
     @Test
-    void contextLoads() {
+    void testCreateIndex() throws IOException {
+        //1.创建索引请求
+        CreateIndexRequest request = new CreateIndexRequest("superj_index");
+        //2.客户端执行请求IndicesClient,请求后获得响应
+        CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
+
+        System.out.println(response);
     }
 
+    //测试获取索引,只能判断存不存在
+    @Test
+    void testExistIndex() throws IOException {
+        //1.创建索引请求
+        GetIndexRequest request = new GetIndexRequest("superj_index");
+        //2.客户端执行请求IndicesClient,请求后获得响应
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+
+        System.out.println(exists);
+    }
+
+    //测试删除索引
+    @Test
+    void testDeleteIndex() throws IOException {
+        //1.创建索引请求
+        DeleteIndexRequest request = new DeleteIndexRequest("superj_index");
+        //2.客户端执行请求IndicesClient,请求后获得响应
+        AcknowledgedResponse delete = client.indices().delete(request, RequestOptions.DEFAULT);
+
+        System.out.println(delete.isAcknowledged());//是否删除成功
+    }
 }
